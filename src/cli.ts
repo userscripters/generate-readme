0295dad86a6daea6f02d148a9ca04c3a177c325a
Oleg Valter <o.a.philippov@gmail.com>
2021-05-30 17:01:29 +0300

    Reworked CLI utilities

--- src/cli.ts
+++ src/cli.ts
@@ -1,100 +1,145 @@
-type BaseArg = {
-  short: string;
-  long: string;
-  description: string;
-  action?: (...args: any[]) => any;
-  passed: boolean;
+import { PackageInfo } from ".";
+import { parseName } from "./utils";
+
+type AnyFunc = (...args: any[]) => any;
+
+type BaseArg<A extends AnyFunc | undefined> = {
+    short: string;
+    long: string;
+    description: string;
+    action?: A;
+    passed: boolean;
 };
 
-type ValueArg = BaseArg & {
-  hasValue: true;
-  defaultValue?: string;
-  value: string;
+class Arg<A extends AnyFunc | undefined> implements BaseArg<A> {
+    short!: string;
+    long!: string;
+    description!: string;
+    action?: A;
+    passed!: boolean;
+
+    constructor(options: BaseArg<A>) {
+        Object.assign(this, options);
+    }
+
+    run(...args: Parameters<Exclude<A, undefined>>) {
+        const { action } = this;
+        return action?.apply(this, args);
+    }
+}
+
+class SimpleArg<A extends AnyFunc | undefined> extends Arg<A> {
+    hasValue: false;
+
+    constructor(options: BaseArg<A>) {
+        super(options);
+        this.hasValue = false;
+    }
+}
+
+class ValueArg<A extends AnyFunc | undefined> extends Arg<A> {
+    hasValue: true;
+    defaultValue?: string;
+    value!: string;
+
+    constructor(
+        options: BaseArg<A> & { defaultValue?: string; value: string }
+    ) {
+        super(options);
+        this.hasValue = true;
+        Object.assign(this, options);
+    }
+}
+
+type Args<A extends AnyFunc | undefined = AnyFunc> = (
+    | SimpleArg<A>
+    | ValueArg<A>
+)[];
+
+type ArgOptions = { defaultValue?: string; action?: AnyFunc };
+
+export type ArgsHash<A extends AnyFunc | undefined = AnyFunc> = {
+    [x: string]: SimpleArg<A> | ValueArg<A>;
 };
-
-type SimpleArg = BaseArg & {
-  hasValue: false;
-};
-
-type Arg = ValueArg | SimpleArg;
-
-type Args = Arg[];
-
-type ArgOptions = { defaultValue?: string; action?: (...args: any[]) => any };
-
-type ArgsHash = { [x: string]: Arg };
 
 const args: Args = [];
 
-export const showHelp = (packageName: string, description: string) => {
-  const describedArgs = args.reduce(
-    (acc, { description, long, short }) =>
-      `${acc}-${short}, --${long}\t\t${description}\n`,
-    ""
-  );
+export const showHelp = ({ description, name }: PackageInfo) => {
+    const { packageName } = parseName(name);
 
-  console.log(`
+    const describedArgs = args.reduce(
+        (acc, { description, long, short }) =>
+            `${acc}-${short}, --${long}\t\t${description}\n`,
+        ""
+    );
+
+    console.log(`
 ${description}
 
 Usage: ${packageName} [options]
 
 All Options:
-
-${describedArgs}
-`);
+${describedArgs}`);
 };
 
-args.push({
-  short: "h",
-  long: "help",
-  description: "show command help",
-  action: showHelp,
-  hasValue: false,
-  passed: false,
-});
+args.push(
+    new SimpleArg({
+        short: "h",
+        long: "help",
+        description: "show command help",
+        action: showHelp,
+        passed: false,
+    })
+);
 
 export const addArg = (
-  long: string,
-  short: string,
-  description: string,
-  { defaultValue, action }: ArgOptions = {}
+    long: string,
+    short: string,
+    description: string,
+    { defaultValue, action }: ArgOptions = {}
 ) => {
-  const base: BaseArg = {
-    long,
-    short,
-    description,
-    action,
-    passed: false,
-  };
+    const base = new Arg({
+        long,
+        short,
+        description,
+        action,
+        passed: false,
+    });
 
-  args.push(
-    defaultValue !== void 0
-      ? { ...base, hasValue: true, defaultValue, value: defaultValue }
-      : { ...base, hasValue: false }
-  );
+    args.push(
+        defaultValue !== void 0
+            ? new ValueArg({ ...base, defaultValue, value: defaultValue })
+            : new SimpleArg(base)
+    );
 };
 
 export const parseArgs = (cliArgs: string[]): ArgsHash => {
-  const mappedToArgs = args.map((arg) => {
-    const { long, short } = arg;
+    const mappedToArgs = args.map((arg) => {
+        const { long, short } = arg;
 
-    const cliArgIdx = cliArgs.findIndex(
-      (cliArg) => cliArg === `--${long}` || cliArg === `-${short}`
-    );
+        const cliArgIdx = cliArgs.findIndex(
+            (cliArg) => cliArg === `--${long}` || cliArg === `-${short}`
+        );
 
-    if (cliArgIdx !== -1) arg.passed = true;
+        if (cliArgIdx !== -1) arg.passed = true;
 
-    const argVal = arg.hasValue
-      ? { ...arg, value: cliArgs[cliArgIdx + 1] || arg.defaultValue || "" }
-      : arg;
+        const argVal = arg.hasValue
+            ? new ValueArg({
+                  ...arg,
+                  value: cliArgs[cliArgIdx + 1] || arg.defaultValue || "",
+              })
+            : new SimpleArg(arg);
 
-    return [long, argVal] as const;
-  });
+        return [long, argVal] as const;
+    });
 
-  return Object.fromEntries(mappedToArgs);
+    return Object.fromEntries(mappedToArgs);
 };
 
-export const getValueArg = (args: ArgsHash, key: string): ValueArg | null => {
-  const arg = args[key];
-  return arg.hasValue ? arg : null;
+export const getValueArg = <A extends AnyFunc | undefined = AnyFunc>(
+    args: ArgsHash<A>,
+    key: string
+): ValueArg<A> | null => {
+    const arg = args[key];
+    return arg.hasValue ? arg : null;
 };
