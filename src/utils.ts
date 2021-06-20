62690fd5e25e66fa2a52bd745f6f3ce7b977288c
Oleg Valter <o.a.philippov@gmail.com>
2021-06-20 19:42:10 +0300

    added object mapper utility

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,18 @@
 import { readFile } from "fs/promises";
 import { PackageInfo } from ".";
 
+export const mapObject = <
+    T extends { [x: string]: any },
+    U extends (key: keyof T, val: T[keyof T]) => any
+>(
+    obj: T,
+    cbk: U
+) => {
+    const output = <{ [P in keyof T]: ReturnType<U> }>{};
+    Object.entries(obj).forEach(([k, v]) => (output[k as keyof T] = cbk(k, v)));
+    return output;
+};
+
 export const getPackage = async (path: string): Promise<PackageInfo | null> => {
     try {
         const contents = await readFile(path, { encoding: "utf-8" });
