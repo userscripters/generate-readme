5002ffec44b9426b940e45fee217e658c2d2064e
Oleg Valter <o.a.philippov@gmail.com>
2021-09-07 16:58:42 +0300

    moved license formatter to own module

---
+++ src/license.ts
@@ -0,0 +1,15 @@
+import { mdLink } from "./utils";
+
+/**
+ * @summary formats a license field
+ * @param {string} [license] license short code
+ * @returns {string}
+ */
+export const formatLicense = (license?: string) => {
+    return license
+        ? mdLink(license, `https://spdx.org/licenses/${license}`)
+        : `Not adopted (see ${mdLink(
+              "GitHub default grant",
+              "https://docs.github.com/en/github/site-policy/github-terms-of-service#5-license-grant-to-other-users"
+          )})`;
+};
--- src/readme.ts
+++ src/readme.ts
@@ -1,26 +1,13 @@
 import { bgRed } from "chalk";
 import { open } from "fs/promises";
 import { PackageInfo } from ".";
+import { formatLicense } from "./license";
 import { mdLink, parseAuthor, scase } from "./utils";
 
 const formatEmail = (email?: string) =>
     email ? `<br>${mdLink(email, `mailto:${email}`)}` : "";
 
 const formatUrl = (url?: string) => (url ? `<br>${mdLink(url, url)}` : "");
-
-/**
- * @summary formats a license field
- * @param {string} [license] license short code
- * @returns {string}
- */
-const formatLicense = (license?: string) => {
-    return license
-        ? mdLink(license, `https://spdx.org/licenses/${license}`)
-        : `Not adopted (see ${mdLink(
-              "GitHub default grant",
-              "https://docs.github.com/en/github/site-policy/github-terms-of-service#5-license-grant-to-other-users"
-          )})`;
-};
 
 export const generateReadme = ({
     author,
